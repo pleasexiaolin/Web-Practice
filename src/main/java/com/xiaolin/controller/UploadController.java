9c150b89c21368162f457e0df59b43a23ec57e58
lzh <pleasexiaolin@163.com>
2025-11-10 22:16:09 +0800

    info：文件上传 user：lin date：2025/11/10 22:15

--- src/main/java/com/xiaolin/controller/UploadController.java
+++ src/main/java/com/xiaolin/controller/UploadController.java
@@ -2,9 +2,7 @@ package com.xiaolin.controller;
 
 import com.xiaolin.common.Result;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
--- src/test/java/com/xiaolin/WebPracticeApplicationTests.java
+++ src/test/java/com/xiaolin/WebPracticeApplicationTests.java
@@ -6,15 +6,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.List;
 import java.util.UUID;
 
 @SpringBootTest
 class WebPracticeApplicationTests {
-
-
-    @Autowired
-    private EmpMapper empMapper;
 
     @Test
     void contextLoads() {
